d5277ed7383d5fa3e2bddc57d8843da94b17711b
Rafa <mirandarafa010@gmail.com>
2024-10-03 12:00:00 -0600

    Añadir archivo_6.java en 2024-10-03T12:00:00

---
+++ archivo_6.java
@@ -0,0 +1,12 @@
+package main;
+
+import java.util.Scanner;
+
+public class Infiriendo {
+    public static void main(String[] args) {
+        var entrada = new Scanner(System.in);
+        var frase = entrada.nextLine();
+        
+        System.out.println("La frase << " + frase + " >> es correcta");
+    }
+}
