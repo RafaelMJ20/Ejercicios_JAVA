78a1561131e4a324aea4f73f55f3b19fdec7b959
Rafa <mirandarafa010@gmail.com>
2024-10-08 12:00:00 -0600

    Añadir archivo_11.java en 2024-10-08T12:00:00

---
+++ archivo_11.java
@@ -0,0 +1,16 @@
+package main;
+
+public class Operadores {
+    public static void main(String[] args) {
+        boolean a, b;
+        
+        /*
+            AND = A && B
+            OR = A || B
+            XOR  = A ^ B
+            NOT = !
+        */
+        a = true;
+        b = false;
+    }
+}
